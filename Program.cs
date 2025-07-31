8959fa0c8e707e879a4b1f708052c2bc2f5ee070
Gabi08-l <senaioloko@gmail.com>
2025-07-31 10:33:44 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        var numero1 = 2;
+        var numero2 = 3;
+
+
+        var soma = numero1 + numero2;
+
+
+
+        Console.WriteLine("Hello, BAHIA!");
+
+        Console.WriteLine($"A soma de {numero1} + {numero2} é igual a {soma}.");
+        //console.writenline("a soma é igual á" + soma)
+    }
+}
